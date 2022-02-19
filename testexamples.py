96452b429ade3f6f73a4be82fad584c92f1a534e
bdatta <bdpd84@gmail.com>
2022-02-19 18:47:44 +0530

    example added

--- testexamples.py
+++ testexamples.py
@@ -3,3 +3,7 @@ foo = (1, 2, 3)
 bar = (4, 5, 6)
 for f, b in zip(foo, bar):
     print(f, b)
+
+input = [[1,2],[3,4],[5,6],[7,8]]
+output = [i[0] for i in input], [i[1] for i in input]
+print (output)
